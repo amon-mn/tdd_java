a8c107ae187ed9371446c2a03d9af0a28979a7a3
amon_mn <menezes.am031@gmail.com>
2023-12-05 20:57:27 -0400

    TDD Java Spring Boot

---
+++ src/main/java/br/com/tddjava/tddjava/modules/courses/entities/Course.java
@@ -0,0 +1,38 @@
+package br.com.tddjava.tddjava.modules.courses.entities;
+
+import java.util.UUID;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "courses")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Course {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+    
+    private UUID id;
+
+    private String description;
+
+    private String name;
+
+    private int workload;
+
+}
